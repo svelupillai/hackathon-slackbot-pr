45dfa5e2b57f57e88445aa5fa29f70e6b8d83909
Erin Benderoff <erin.benderoff@appdirect.com>
2021-05-27 15:35:53 -0400

    fix after rebase

--- src/main/java/com/application/GithubReminder.java
+++ src/main/java/com/application/GithubReminder.java
@@ -1,3 +1,5 @@
+package com.application;
+
 import java.io.IOException;
 import java.util.Date;
 
--- src/main/java/com/application/SlackApp.java
+++ src/main/java/com/application/SlackApp.java
@@ -1,53 +1,61 @@
 package com.application;
 
-import com.slack.api.bolt.App;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.slack.api.bolt.App;
+
 @Configuration
 public class SlackApp {
-    @Bean
-    public App initSlackApp() {
-        App app = new App();
-        app.command("/prpal", (req, ctx) -> {
-            String payload = req.getPayload().getText();
+	@Bean
+	public App initSlackApp() {
+		App app = new App();
 
-            if (payload == null) {
-                return ctx.ack("Error: Must provide at least one argument to PrPal");
-            }
+		final String USER_TOKEN = System.getenv("SLACK_USER_TOKEN");
 
-            String[] commandArgs = req.getPayload().getText().split(" ");
-            System.out.println(commandArgs[0].toUpperCase());
-            switch(commandArgs[0].toUpperCase()) {
-                case Constants.SUBSCRIBE:
-                    if (commandArgs.length < 2) {
-                        return ctx.ack("Error: Must provide the repository name as the second argument");
-                    }
-                    String repositoryName = commandArgs[1];
-                    return GithubSubscribe.subscribeToRepository(ctx, repositoryName);
-                case Constants.FOLLOW:
-                    if (commandArgs.length < 2) {
-                        return ctx.ack("Error: Must provide a Github username as the second argument");
-                    }
-                    String githubUsername = commandArgs[1];
-                    return GithubSubscribe.followUser(ctx, githubUsername);
-                case Constants.HELP:
-                    return ctx.ack(Help.getHelpText());
+		app.command("/prpal", (req, ctx) -> {
+			String payload = req.getPayload().getText();
 
-                case Constants.PING:
-                    if (commandArgs.length < 3) {
-                        return ctx.ack("Error: Must provide a comma separated list of names and link to PR");
-                    }
+			if (payload == null) {
+				return ctx.ack("Error: Must provide at least one argument to PrPal");
+			}
 
-                    String result = Ping.pingForReview(ctx, commandArgs[1], commandArgs[2]);
-                    if(result.length() > 0){
-                        return ctx.ack(result);
-                    }
-                    return ctx.ack();
-                default:
-                    return ctx.ack("Invalid command: " + commandArgs[0]);
-            }
-        });
-        return app;
-    }
+			String[] commandArgs = req.getPayload().getText().split(" ");
+			System.out.println(commandArgs[0].toUpperCase());
+			switch(commandArgs[0].toUpperCase()) {
+				case Constants.SUBSCRIBE:
+					if (commandArgs.length < 2) {
+						return ctx.ack("Error: Must provide the repository name as the second argument");
+					}
+					String repositoryName = commandArgs[1];
+					return GithubSubscribe.subscribeToRepository(ctx, repositoryName);
+				case Constants.FOLLOW:
+					if (commandArgs.length < 2) {
+						return ctx.ack("Error: Must provide a Github username as the second argument");
+					}
+					String githubUsername = commandArgs[1];
+					return GithubSubscribe.followUser(ctx, githubUsername);
+				case Constants.HELP:
+					return ctx.ack(Help.getHelpText());
+				case Constants.PING:
+					if (commandArgs.length < 3) {
+						return ctx.ack("Error: Must provide a comma separated list of names and link to PR");
+					}
+
+					String result = Ping.pingForReview(ctx, commandArgs[1], commandArgs[2]);
+					if(result.length() > 0){
+						return ctx.ack(result);
+					}
+					return ctx.ack();
+				case Constants.REMIND:
+					if (commandArgs.length < 4) {
+						return ctx.ack("Error: Must provide command in the format: remind <PR_LINK> <hours> <minutes>");
+					}
+					return GithubReminder.remind(ctx, USER_TOKEN, commandArgs[1], commandArgs[2], commandArgs[3]);
+				default:
+					return ctx.ack("Invalid command: " + commandArgs[0]);
+			}
+		});
+		return app;
+	}
 }

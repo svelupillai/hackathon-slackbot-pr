7c548887d0c34908dc0846285653c6f1cff926ab
corey.levine <corey.levine@appdirect.com>
2021-05-27 18:19:53 -0400

    Clean code

--- src/main/java/com/application/WebhookApiImpl.java
+++ src/main/java/com/application/WebhookApiImpl.java
@@ -1,5 +1,6 @@
 package com.application;
 
+import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.application.webhookEvents.BaseEvent;
-import com.application.webhookEvents.EventAction;
 import com.application.webhookEvents.PullRequestEvent;
 import com.application.webhookEvents.PullRequestReviewCommentEvent;
 import com.google.gson.Gson;
@@ -21,35 +21,13 @@ public class WebhookApiImpl {
 			consumes = { "application/json" },
 			method = RequestMethod.POST)
 	public ResponseEntity<String> payload(HttpEntity<String> httpEntity) {
-		// do some magic!
 		String json = httpEntity.getBody();
-		EventAction action = new Gson().fromJson(json, EventAction.class);
 		BaseEvent evnt = null;
-		switch (action.getAction()){
-			case "assigned":
-			case "auto_merge_disabled":
-			case "auto_merge_enabled":
-			case "closed":
-			case "converted_to_draft":
-			case "labeled":
-			case "locked":
-			case "opened":
-			case "ready_for_review":
-			case "reopened":
-			case "review_request_removed":
-			case "review_requested":
-			case "synchronize":
-			case "unassigned":
-			case "unlabeled":
-			case "unlocked":
-				evnt = new Gson().fromJson(json, PullRequestEvent.class);
-				break;
-			case "created":
-			case "edited":
-			case "deleted":
-				evnt = new Gson().fromJson(json, PullRequestReviewCommentEvent.class);
-				break;
-		}
+		JSONObject jsonObject = new JSONObject(json);
+
+		if (jsonObject.has("number"))
+			evnt = new Gson().fromJson(json, PullRequestEvent.class);
+		if (jsonObject.has("comment"))
 
 		System.out.println(evnt);
 		return new ResponseEntity<String>(HttpStatus.OK);

9d76c4517cd9aa44dbbaea55e49629de54fed8f3
alinekof <aline.koftikian@appdirect.com>
2021-05-27 18:15:19 -0400

    Added calls to database in GithubSubscribe class

--- src/main/java/com/application/GithubSubscribe.java
+++ src/main/java/com/application/GithubSubscribe.java
@@ -1,17 +1,79 @@
 package com.application;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import com.database.Controller.DatabaseController;
+import com.database.Model.User;
+import com.database.Util.UserAdapter;
 import com.slack.api.bolt.context.builtin.SlashCommandContext;
 import com.slack.api.bolt.response.Response;
 
 public class GithubSubscribe {
 
+	private static DatabaseController databaseController;
+
 	public static Response subscribeToRepository(SlashCommandContext ctx, String repositoryName) {
 		// TODO Call GH API to subscribe to repository and persist in db
+
+		//Persist subscription in database
+		databaseController = new DatabaseController();
+		User userQueried = UserAdapter.DocumentToUser(databaseController.getUser("userId"));
+
+		if(Objects.isNull(userQueried)){
+			//Create new user
+			User newUser = new User();
+			newUser.setUserId("userId"); //TODO: Remove hardcoded param, use real id from req object (apply everywhere in this class)
+
+			List<String> subscribedRepoIds = new ArrayList<>();
+			subscribedRepoIds.add(repositoryName);
+			newUser.setSubscribedRepoIds(subscribedRepoIds);
+
+			databaseController.createUser(UserAdapter.userToDocument(newUser));
+		} else {
+			//Get user + Update with new repo name
+			List<String> subscribedRepoIds = userQueried.getSubscribedRepoIds();
+			subscribedRepoIds = Optional.ofNullable(subscribedRepoIds)
+				.orElse(new ArrayList<>());
+			subscribedRepoIds.add(repositoryName);
+			userQueried.setSubscribedRepoIds(subscribedRepoIds);
+
+			databaseController.updateUser("userId", UserAdapter.userToDocument(userQueried));
+		}
+
 		return ctx.ack( "Successfully subscribed to repository " + repositoryName);
 	}
 
 	public static Response followUser(SlashCommandContext ctx, String username) {
 		// TODO Call GH API to follow a user's activity and persist in db
+
+		//Persist subscription in database
+		databaseController = new DatabaseController();
+		User userQueried = UserAdapter.DocumentToUser(databaseController.getUser("userId"));
+
+		if(Objects.isNull(userQueried)){
+			//Create new user
+			User newUser = new User();
+			newUser.setUserId("userId"); //TODO: Remove hardcoded param, use real id from req object (apply everywhere in this class)
+
+			List<String> followedUserIds = new ArrayList<>();
+			followedUserIds.add(username);
+			newUser.setSubscribedUserIds(followedUserIds);
+
+			databaseController.createUser(UserAdapter.userToDocument(newUser));
+		} else {
+			//Get user + Update with new repo name
+			List<String> subscribedUserIds = userQueried.getSubscribedUserIds();
+			subscribedUserIds = Optional.ofNullable(subscribedUserIds)
+				.orElse(new ArrayList<>());
+			subscribedUserIds.add(username);
+			userQueried.setSubscribedUserIds(subscribedUserIds);
+
+			databaseController.updateUser("userId", UserAdapter.userToDocument(userQueried));
+		}
+
 		return ctx.ack( "Successfully following Github user " + username);
 	}
 }
--- src/main/java/com/database/Util/UserAdapter.java
+++ src/main/java/com/database/Util/UserAdapter.java
@@ -3,6 +3,7 @@ package com.database.Util;
 import static com.database.Util.Constants.*;
 
 import java.util.List;
+import java.util.Objects;
 
 import org.bson.Document;
 
@@ -11,13 +12,19 @@ import com.database.Model.User;
 public class UserAdapter {
 
 	public static Document userToDocument(User user) {
+		if(Objects.isNull(user)) {
+			return null;
+		}
 		return new Document(DB_FIELD_USER_ID, user.getUserId())
 			.append(DB_FIELD_SUBSCRIBED_REPO_IDS, user.getSubscribedRepoIds())
 			.append(DB_FIELD_SUBSCRIBED_USER_IDS, user.getSubscribedUserIds())
 			.append(DB_FIELD_INTERVAL_TIME_IN_MIN, user.getIntervalTimeInMinutes());
 	}
 
-	public static User DBObjectToUser(Document document) {
+	public static User DocumentToUser(Document document) {
+		if(Objects.isNull(document)) {
+			return null;
+		}
 		User user = new User();
 		user.setUserId((String) document.get(DB_FIELD_USER_ID));
 		user.setSubscribedRepoIds((List<String>) document.get(DB_FIELD_SUBSCRIBED_REPO_IDS));

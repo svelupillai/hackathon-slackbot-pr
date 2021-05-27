ade9deb87052c1bc994dfc25b845b30d532a74cf
Erin Benderoff <erin.benderoff@appdirect.com>
2021-05-27 16:17:54 -0400

    help text

--- src/main/java/com/application/Help.java
+++ src/main/java/com/application/Help.java
@@ -3,7 +3,11 @@ package com.application;
 public class Help {
 
 	public static String getHelpText() {
-		// TODO Return a message listing all possible commands
-		return "Here are all the possible commands: ...";
+		return "Usage: `/prpal <command> <args...>`\n\tCommands:\n\t\t" +
+			"`subscribe <repository_name>` -> Subscribe to a repository\n\t\t" +
+			"`follow <github_username>` -> Follow a Github user\n\t\t" +
+			"`ping <comma_separated_list_of_slack_usernames> <link_to_PR>` -> Ping user(s) to review a PR\n\t\t" +
+			"`remind <link_to_PR> <hours> <minutes>` -> Set yourself a reminder to review a PR in <hours> hours and <minutes> mins\n\t\t" +
+			"`help` -> Display this help text";
 	}
 }
